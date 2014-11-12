7a6d27b6dc301e4224789d143df377eb092fa4ed
Falko Richter <falko@briefhansa.de>
2014-11-12 19:33:30 +0100

    working sensorberg name provider

--- sensorscanner/src/main/java/com/sensorberg/android/sensorscanner/nameProvider/SensorbergNameProvider.java
+++ sensorscanner/src/main/java/com/sensorberg/android/sensorscanner/nameProvider/SensorbergNameProvider.java
@@ -6,7 +6,7 @@ import com.sensorberg.sdk.cluster.BeaconId;
 public class SensorbergNameProvider implements NameProvider{
     @Override
     public boolean provideName(BeaconId beaconId, BeaconName beaconName) {
-        if (beaconId.getNormalizedUUIDString().contains("7367672374000000ffff0000ffff000")){
+        if (beaconId.getNormalizedUUIDString().contains("7367672374000000FFFF0000FFFF000")){
             beaconName.first = beaconId.toTraditionalString().replace("73676723-7400-0000-ffff-0000ffff000","SB-");
             return false;
         }
