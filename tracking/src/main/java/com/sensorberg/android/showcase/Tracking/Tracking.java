b792789757992b43de047c8f5bd8b9667bbca5c2
Falko Richter <falko@briefhansa.de>
2014-12-04 15:34:21 +0100

    tracking is a package

---
+++ tracking/src/main/java/com/sensorberg/android/showcase/Tracking/Tracking.java
@@ -0,0 +1,59 @@
+package com.sensorberg.android.showcase.Tracking;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+public interface Tracking extends Application.ActivityLifecycleCallbacks {
+
+    public static final Tracking NONE = new Tracking() {
+        @Override
+        public void logEvent(String name) {
+
+        }
+
+        @Override
+        public void logEvent(String eventName, boolean checked) {
+
+        }
+
+        @Override
+        public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+        }
+
+        @Override
+        public void onActivityStarted(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityResumed(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityPaused(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityStopped(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+        }
+
+        @Override
+        public void onActivityDestroyed(Activity activity) {
+
+        }
+    };
+
+    public void logEvent(String name);
+
+    void logEvent(String eventName, boolean checked);
+}
